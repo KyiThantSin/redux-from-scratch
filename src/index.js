3979f5e75d7a0889e86b4a3fbc6cc1ac0f057c55
KyiThantSin <kyithantsin@mtu.edu.mm>
2022-06-10 11:50:15 +0630

    dispatching actions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+import store from "./store";
+
+const unsubscribe = store.subscribe(()=>{
+    console.log("Store Changed", store.getState());
+})
+
+store.dispatch({
+    //action
+    type:"bugAdded",
+    payload:{
+        description:"Bug1"
+    }
+})
+
+unsubscribe();
+
+store.dispatch({
+    type:"bugRemoved",
+    payload:{
+        id:1
+    }
+})
+
+//console.log(store);
+
